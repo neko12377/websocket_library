61a92525ad43f1db0e81ed1d3995176e5ac59d6d
Chen Kai Po <neko12377+github@gmail.com>
2023-02-25 23:35:10 +0800

    established WebSocket connection

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use std::net::{
     TcpListener,
     TcpStream,
 };
+use sha1::{ Digest, Sha1 };
+use base64::{Engine as _, engine::general_purpose};
 
 fn main() -> std::io::Result<()>{
 
@@ -13,17 +15,16 @@ fn main() -> std::io::Result<()>{
 
     let listener = TcpListener::bind(&addrs[..]).expect("Should be listener");
 
-    // loop {
-    match listener.accept() {
-        Ok((socket, addr)) => {
-            println!("new client: {addr:?}");
-            println!("stream is {socket:?}");
-            handle_connection(socket);
-        },
-        Err(e) => println!("couldn't get client: {e:?}"),
-    };
-    // }
-    Ok(())
+    loop {
+        match listener.accept() {
+            Ok((socket, addr)) => {
+                println!("new client: {addr:?}");
+                println!("stream is {socket:?}");
+                handle_connection(socket);
+            },
+            Err(e) => println!("couldn't get client: {e:?}"),
+        };
+    }
 }
 
 fn handle_connection(mut socket: TcpStream) {
@@ -39,6 +40,32 @@ fn handle_connection(mut socket: TcpStream) {
         .iter()
         .filter(|h| h.contains("Sec-WebSocket-Key"))
         .collect::<Vec<_>>();
-    println!("{key:?}");
-    socket.write(b"response").expect("Should repound to incoming request");
+    if !key.is_empty() {
+        println!("WebSocket handshake detected with key: {key:?}");
+        let key = key[0].split(" ").collect::<Vec<&str>>()[1];
+        println!("key vlaue = {key:?}");
+        handle_socket_response(&mut socket, key);
+    } else {
+        println!("Aborting non-WebSocket connection");
+        socket.write(b"response").expect("Should repound to incoming request");
+        return
+    }
+
+}
+
+fn handle_socket_response(socket:&mut TcpStream, key: &str) {
+    // Concatenate key with 258EAFA5-E914-47DA-95CA-C5AB0DC85B11 (RFC 6455)
+    // Take SHA-1 hash of this string to obtain hash value
+    const WS_GUID: &[u8] = b"258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+    let key = key.as_bytes();
+    let mut hasher = Sha1::default();
+    hasher.update(key);
+    hasher.update(WS_GUID);
+    // Encode hash value with base-64  
+    let res = general_purpose::STANDARD.encode(&hasher.finalize());
+    println!("Sec-WebSOcket-Accept: {res:?}");
+
+    socket.write_all(
+        format!("HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: {}\r\n\r\n", res).as_bytes()
+    ).expect("Should write handshake response");
 }
